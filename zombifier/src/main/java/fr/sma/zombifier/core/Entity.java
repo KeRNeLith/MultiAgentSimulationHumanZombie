b83737119ef7eec8df4793b3dbe2dd37e55fe815
Ace-Nanter <ace.nanter@hotmail.fr>
2015-12-15 11:50:44 +0100

    Creation des fichiers csv + Implementation constructeur Entity

--- zombifier/src/main/java/fr/sma/zombifier/core/Entity.java
+++ zombifier/src/main/java/fr/sma/zombifier/core/Entity.java
@@ -7,13 +7,23 @@ import fr.sma.zombifier.behavior.IBehaviour.BehaviourType;
  *
  * @author Alexandre Rabérin
  */
-public class Entity
+public abstract class Entity
 {
     private IBehaviour m_behaviour;
     private BehaviourType m_behaviourType;
+    private Platform m_position;
+    private Pair<int, int> m_direction;
     
-    public Entity()
+    /**
+     * Cree une instance Entité
+     * @param p Objet plateforme indiquant la position de l'entité
+     * @param direction_x Direction du regard sur l'axe des abscisses
+     * @param direction_y Direction du regard sur l'axe des ordonnées
+     */
+    public Entity(Platform p, final int direction_x, final int direction_y)
     {
-        
+        this.m_position = p;
+        this.m_direction = new Pair<int,int>(direction_x, direction_y); 
+        this.m_mt = new MersenneTwisterFast(m_base_seed++);
     }
 }
